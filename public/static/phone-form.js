814a603f157cb3ad16f01d5e987469de27f8fa9c
Joshmossass <joshmsosso@gmail.com>
2018-12-29 17:38:23 -0600

    add keypad page

---
+++ public/static/phone-form.js
@@ -0,0 +1,1 @@
+let phoneNumber = document.getElementById("customerPhoneNumber");
--- public/static/test.js
+++
@@ -1,3 +0,0 @@
-let amountField = document.querySelector("#donationAmount")
-let customAmount = document.querySelector("#customAmount");
-let customAmountInput = document.querySelector("#customAmountField")
